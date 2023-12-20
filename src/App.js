359265fac4ec0b656c0a6b1699de4b3e7af9915f
SRIRAMPRO <sriram49322@gmail.com>
2023-12-20 09:58:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Plantmain from './components/Plantmain';
+
+
+
+function App() {
+  return (
+    <div>
+     <Plantmain />
+    </div>
+  );
+}
+
+export default App;
